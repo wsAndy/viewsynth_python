9fbfdd8f0a206bff12dba01a0b5d5d1814446ffb
wsAndy <shengwangandy@foxmail.com>
2018-01-22 22:51:53 +0800

    add createSpflag, need to do saveDpethImage

--- synth_src/createInfo.py
+++ synth_src/createInfo.py
@@ -14,6 +14,38 @@ p3d = cam_data[2]
 visTable = cam_data[3]
 
 
+def createSpflag(slic_label_name, dmap_name):
+    label = np.load(slic_label_name)
+    dmap = np.load(dmap_name)
+
+    Nsp = label.max()
+    Pcnt = label.sum()
+    th = 0.1*Pcnt/Nsp
+    Spflag = np.zeros([Nsp,1])
+    for i in range(0,Nsp):
+        # find region whose ID=i
+        # judge id the dmap in this region has enough depth point
+        # true: set spflag[i] = 1
+        # False: spflag[i] = 0
+        sp_ind = np.where(label == i)
+        dep_point_count = 0
+        for dep_ind in range(0,sp_ind[0].shape[0]):
+            # pixel( sp_ind[dep] )
+            sp_x = sp_ind[0][dep_ind]
+            sp_y = sp_ind[1][dep_ind]
+            # judge if enough depth point occurs in this region
+            if abs(dmap[sp_x][sp_y]) > 0.1:
+                dep_point_count = dep_point_count + 1
+
+        if dep_point_count < th:
+            Spflag[i] = 1
+
+
+def saveDepth(x):
+    # need project 3d to 2d
+    # To Do
+    pass
+
 def saveSlic(x):
     print('Start img={0}'.format(x))
     time1 = time.time()
@@ -25,6 +57,8 @@ def saveSlic(x):
     # 直接存储没有意义，因为这边使用像素值表示superpixel的ID
     # io.imsave('./slic/seg' + str(x) + '.png', seg)
     # cv2.imwrite('./slic/segcv2_'+str(x)+'.png',seg)
+
+
     print('End: {0}'.format(time.time() - time1))
 
 
@@ -39,12 +73,9 @@ if __name__ == '__main__':
         pool.close()
         pool.join()
 
+    for i in range(0,8):
+        createSpflag('./slic/seg'+str(i)+'.npy','./depth/dep'+str(i)+'.npy')
 
     print('Done.')
-
-
-
-
-
 
 
